5514143b3ceba1915adcc86d6ca5cfbac6903d0f
yu <cosJin@live.cn>
2019-08-11 21:50:41 +0800

    l

--- try.py
+++ try.py
@@ -1,7 +1,8 @@
-import heapq
-nums = [21, 3, 45, 23, 12]
-# nums[:3] = nums[:3].sort()
-print(nums) 
-for i in range(len(nums)):
-    print(i)
-print(i)
+# import heapq
+# nums = [21, 3, 45, 23, 12]
+# # nums[:3] = nums[:3].sort()
+# print(nums) 
+# for i in range(len(nums)):
+#     print(i)
+# print(i)
+'11111111'.fromBinaryToInt()
