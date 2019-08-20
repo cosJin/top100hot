531bdad08140617eebb7b128177672eae88670a3
yu <cosJin@live.cn>
2019-08-20 22:33:27 +0800

    62

--- 62. Unique Paths.py
+++ 62. Unique Paths.py
@@ -5,4 +5,22 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
-        
+        if m == 1 or n == 1: return 1
+        dp = [[0 for _ in range(n)] for _ in range(m)]
+        dp[-1][-1] = 1
+        def bfs(next):
+            nex = set()
+            if len(next) == 1:
+                return dp[0][1] + dp[1][0]
+            for point in next:
+                dp[point[0]][point[1]] = (0 if point[0] == m-1 else dp[point[0]+1][point[1]]) + (0 if point[1] == n-1 else dp[point[0]][point[1] + 1])
+                if point[0] != 0: nex.add((point[0] - 1, point[1])) 
+                if point[1] != 0: nex.add((point[0], point[1] - 1)) 
+            return bfs(nex)
+        return bfs(((m-2,n-1),(m-1,n-2)))
+a = Solution()
+print(a.uniquePaths(3,2))
+    
+
+
+
--- try.py
+++
@@ -1,2 +0,0 @@
-b = int("b") - int("a")
-print(b)
