0b38cca28a83f207d2240547b12576a3d51254c1
yuxuejin@sogou <yuxuejin@sogou-inc.com>
2019-08-27 12:00:42 +0800

    79over 学到了dfs遍历二维数组定义dx dy

--- 79. Word Search.py
+++ 79. Word Search.py
@@ -5,45 +5,32 @@ class Solution(object):
         :type word: str
         :rtype: bool
         """
+        if not board:return False
+        dx = [-1,1,0,0]
+        dy = [0,0,-1,1]
         self.res = False
-        tag = [[0 for _ in board[0]] for _ in board]
-        def bfs(target,i,j):
+        def dfs(target,i,j,board):
             if target == "":
                 self.res = True
                 return 
-            if i == 0 and board[i+1][j] == target[0] and tag[i+1][j] == 0:
-                tag[i+1][j] = 1
-                bfs(target[1:],i+1,j)
-            elif i == len(board)-1 and board[i-1][j] == target[0] and tag[i-1][j] == 0: 
-                tag[i-1][j] = 1
-                bfs(target[1:],i-1,j)
-            elif i != 0 and i != len(board)-1:
-                if board[i-1][j] == target[0] and tag[i-1][j] == 0:
-                    tag[i-1][j] = 1
-                    bfs(target[1:],i-1,j)
-                if board[i+1][j] == target[0] and tag[i+1][j] == 0:
-                    tag[i+1][j] = 1
-                    bfs(target[1:],i+1,j)
-            if j == 0 and board[i][j+1] == target[0] and tag[i][j+1] == 0:
-                tag[i][j+1] = 1
-                bfs(target[1:],i,j+1)
-            elif j == len(board[0])-1 and board[i][j-1] == target[0] and tag[i][j-1] == 0: 
-                tag[i][j-1] = 1
-                bfs(target[1:],i,j-1)
-            elif j != 0 and j != len(board[0])-1:
-                if board[i][j-1] == target[0] and tag[i][j-1] == 0:
-                    tag[i][j-1] = 1
-                    bfs(target[1:],i,j-1)
-                if board[i][j+1] == target[0] and tag[i][j+1] == 0:
-                    tag[i][j+1] = 1
-                    bfs(target[1:],i,j+1)
-        bfs(word,0,0)
+            for k in range(4):
+                x = i + dx[k]
+                y = j + dy[k]
+                if 0 <= x <= len(board)-1 and 0 <= y <= len(board[0])-1 and self.res != True: #这里判断res很重要，可以提高速度。
+                    if board[x][y] == target[0]:
+                        t = board[i][j]
+                        board[i][j] = "#"
+                        dfs(target[1:],x,y,board)
+                        board[i][j] = t
+        for i in range(len(board)):
+            for j in range(len(board[0])):
+                if board[i][j] == word[0]:
+                    dfs(word[1:],i,j,board)
         return self.res
 a = Solution()
-print(a.exist([
-  ['A','B','C','E'],
-  ['S','F','C','S'],
-  ['A','D','E','E']],"BCCED"))
+print(a.exist([["A","B","C","E"],
+               ["S","F","E","S"],
+               ["A","D","E","E"]], "ABCESEEEFS"))
 
 
             
--- try.py
+++ try.py
@@ -1,2 +1,4 @@
 a = [[1,2,3],[4,5,6],[7,8,9]]
-print(a.index(5))
+b = [aa[:] for aa in a]
+b[0][0] = 9
+print(a)
